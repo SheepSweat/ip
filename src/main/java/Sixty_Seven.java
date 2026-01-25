d7e7c3659ee626d265cf861d35e9294f46c44641
E1398615@u.nus.edu <E1398615@u.nus.edu>
2026-01-26 01:22:13 +0800

    Changed chatbot name + Implement skeleton version

---
+++ src/main/java/Sixty_Seven.java
@@ -0,0 +1,11 @@
+public class Sixty_Seven {
+    public static void main(String[] args) {
+        System.out.println("━".repeat(60));
+        System.out.println("Hello! I'm Sixty_Seven");
+        System.out.println("What can I do for you?");
+        System.out.println("━".repeat(60));
+        System.out.println(" Bye. Hope to see you again soon!\n");
+        System.out.println("━".repeat(60));
+
+    }
+}
