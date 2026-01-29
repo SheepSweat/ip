adc5849dc3ef4767c561298fa1e0c27aa9779418
E1398615@u.nus.edu <E1398615@u.nus.edu>
2026-01-30 05:12:00 +0800

    Mark as done function: bot is able to mark task as done to keep track of progress
    Allows bot the ability to mark tasks as done, as well as the ability to change the status back to not done.

--- src/main/java/Sixty_Seven.java
+++ src/main/java/Sixty_Seven.java
@@ -1,5 +1,27 @@
 import java.util.Scanner;
 public class Sixty_Seven {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+        public String getDescription() {
+            return description;
+        }
+        public void SetIsDone() {
+            this.isDone = true;
+        }
+        public void SetIsUndone() {
+            this.isDone = false;
+        }
+    }
+
     public static void printEnding(){
         //System.out.println("━".repeat(60));
         System.out.println(" Bye. Hope to see you again soon!\n");
@@ -12,7 +34,7 @@ public class Sixty_Seven {
         System.out.println("What can I do for you?");
         System.out.println("━".repeat(60));
         String EchoedLine;
-        String[] List = new String[100];
+        Task[] List = new Task[100];
         int NumberofItems = 0;
         Scanner input = new Scanner(System.in);
         do {
@@ -23,15 +45,33 @@ public class Sixty_Seven {
                 for (int i=0;i<NumberofItems;i++)
                 {
                     //System.out.println("━".repeat(60));
-                    System.out.println( (i+1) + ". "+ List[i]);
+                    String status = List[i].getStatusIcon();
+                    System.out.println( (i+1) + ".[" + status + "] " + List[i].getDescription());
                 }
                 System.out.println("━".repeat(60));
             }
             else if(!EchoedLine.equals("bye")){
-                List[NumberofItems] = EchoedLine;
-                System.out.println("added: " + EchoedLine);
-                System.out.println("━".repeat(60));
-                NumberofItems++;
+                if (EchoedLine.startsWith("mark"))
+                {
+                    String[] parts = EchoedLine.split(" ");
+                    int TaskID = Integer.parseInt(parts[1]);
+                    List[TaskID-1].SetIsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  [" + List[TaskID-1].getStatusIcon() + "] " + List[TaskID-1].getDescription());
+                    System.out.println("━".repeat(60));
+                } else if (EchoedLine.startsWith("unmark")) {
+                    String[] parts = EchoedLine.split(" ");
+                    int TaskID = Integer.parseInt(parts[1]);
+                    List[TaskID-1].SetIsUndone();
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("  [" + List[TaskID-1].getStatusIcon() + "] " + List[TaskID-1].getDescription());
+                    System.out.println("━".repeat(60));
+                }else {
+                    List[NumberofItems] = new Task(EchoedLine);
+                    System.out.println("added: " + EchoedLine);
+                    System.out.println("━".repeat(60));
+                    NumberofItems++;
+                }
             }
         } while(!EchoedLine.equals("bye"));
         printEnding();
