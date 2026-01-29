e6daeebf0879f14ce614497a21d717df0199ec0a
E1398615@u.nus.edu <E1398615@u.nus.edu>
2026-01-30 04:23:48 +0800

    Echo function: make bot repeat user input
    Allows bot to echo commands entered by the user, and exits when the user types the command "bye".

--- src/main/java/Sixty_Seven.java
+++ src/main/java/Sixty_Seven.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
 public class Sixty_Seven {
+    public static void printEnding(){
+        //System.out.println("━".repeat(60));
+        System.out.println(" Bye. Hope to see you again soon!\n");
+        System.out.println("━".repeat(60));
+    }
+
     public static void main(String[] args) {
         System.out.println("━".repeat(60));
         System.out.println("Hello! I'm Sixty_Seven");
         System.out.println("What can I do for you?");
         System.out.println("━".repeat(60));
-        System.out.println(" Bye. Hope to see you again soon!\n");
-        System.out.println("━".repeat(60));
-
+        String EchoedLine;
+        Scanner input = new Scanner(System.in);
+        do {
+            EchoedLine = input.nextLine();
+            System.out.println("━".repeat(60));
+            System.out.println(EchoedLine);
+            System.out.println("━".repeat(60));
+        }while(!EchoedLine.equals("bye"));
+        printEnding();
     }
 }
