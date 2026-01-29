5a9d524726537a3f11ce1c9e4be6d7db932a5e9a
E1398615@u.nus.edu <E1398615@u.nus.edu>
2026-01-30 04:33:49 +0800

    Add and List function: bot is able to store and repeat user input
    Allows bot the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Sixty_Seven.java
+++ src/main/java/Sixty_Seven.java
@@ -12,13 +12,28 @@ public class Sixty_Seven {
         System.out.println("What can I do for you?");
         System.out.println("━".repeat(60));
         String EchoedLine;
+        String[] List = new String[100];
+        int NumberofItems = 0;
         Scanner input = new Scanner(System.in);
         do {
             EchoedLine = input.nextLine();
             System.out.println("━".repeat(60));
-            System.out.println(EchoedLine);
-            System.out.println("━".repeat(60));
-        }while(!EchoedLine.equals("bye"));
+            if(EchoedLine.equals("list"))
+            {
+                for (int i=0;i<NumberofItems;i++)
+                {
+                    //System.out.println("━".repeat(60));
+                    System.out.println( (i+1) + ". "+ List[i]);
+                }
+                System.out.println("━".repeat(60));
+            }
+            else if(!EchoedLine.equals("bye")){
+                List[NumberofItems] = EchoedLine;
+                System.out.println("added: " + EchoedLine);
+                System.out.println("━".repeat(60));
+                NumberofItems++;
+            }
+        } while(!EchoedLine.equals("bye"));
         printEnding();
     }
 }
